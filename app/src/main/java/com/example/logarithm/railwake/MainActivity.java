35f97cd364b59bcc4846c10c701944ae1ef2cda1
logarithm <iamrohan04@gmail.com>
2018-08-15 12:10:51 +0530

    Added  Scroll bar to Main  Menu

--- app/src/main/java/com/example/logarithm/railwake/MainActivity.java
+++ app/src/main/java/com/example/logarithm/railwake/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         pnrCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello This", Toast.LENGTH_SHORT).show();
+                pnrStatus(v);
             }
         });
 
